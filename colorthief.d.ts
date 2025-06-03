6e16666cfbb45c0dfbbd36c98f81979aa145f1c1
zhanghuaiwei <zhanghw@sxdata.com.cn>
2025-06-03 10:19:42 +0800

    fix: 集成lint

--- colorthief.d.ts
+++ colorthief.d.ts
@@ -1,9 +1,9 @@
 declare module 'colorthief' {
-    class ColorThief {
-        constructor()
-        getColor(sourceImage: HTMLImageElement, quality?: number): Promise<[number, number, number]>
-        getPalette(sourceImage: HTMLImageElement, colorCount?: number, quality?: number): Promise<[number, number, number][]>
-    }
+  class ColorThief {
+    constructor()
+    getColor(sourceImage: HTMLImageElement, quality?: number): Promise<[number, number, number]>
+    getPalette(sourceImage: HTMLImageElement, colorCount?: number, quality?: number): Promise<[number, number, number][]>
+  }
 
-    export default ColorThief
+  export default ColorThief
 }
